84907d30a63da8b0d89f29be6826eb2e07f282a0
Demi <kirklandnokia@uskirkdhcp13136.americas.nokia.com>
2009-06-02 16:01:23 -0700

    able to create audio file - not yet populated with data

--- Classes/AudioQueue.h
+++ Classes/AudioQueue.h
@@ -8,14 +8,26 @@
 
 #import <Cocoa/Cocoa.h>
 #import <MTCoreAudio/MTCoreAudio.h>
+#import <AudioToolbox/AudioToolbox.h>
 
 @class MTCoreAudioDevice;
 
 @interface AudioQueue : NSObject 
 {
 	AudioStreamBasicDescription * basicStreamDescription;
+	FSRef * audioFileDirFSPathRef;
+	FSRef * audioFileFSPathRef;
+	AudioFileID audioFileIDHandle;
+	CFStringRef audioFileName;
+	AudioFileTypeID audioFileType;
 	MTCoreAudioDevice *inputDevice;
 }
+
+- (void) _setUpAudioFileData: (MTCoreAudioDevice *) device;
+- (void) _setUpFSRef;
+- (void) _setUpCFStringRef;
+- (void) _createNewAudioFile;
+- (void) _setUpStreamDescription: (MTCoreAudioDevice *) device;
 
 - (void) startRecording;
 - (void) stopRecording;
--- Classes/AudioQueue.m
+++ Classes/AudioQueue.m
@@ -17,53 +17,105 @@
 	NSLog(@"Allocating audioqueue");
 	if (self = [super init])
 	{
-		int index;
-		basicStreamDescription = (AudioStreamBasicDescription *)calloc(1, sizeof(AudioStreamBasicDescription));
 		NSLog(@"SETTING INPUT");
 		//MTCoreAudioDevice * device = [[MTCoreAudioDevice devicesWithName:@"Built-in Input" havingStreamsForDirection:kMTCoreAudioDeviceRecordDirection] objectAtIndex:0];
 		MTCoreAudioDevice * device = [MTCoreAudioDevice defaultInputDevice];
 		NSLog([device description]);
-		uint32 channels = [device channelsForDirection:kMTCoreAudioDeviceRecordDirection];
-		NSLog(@"device channels: %i", channels);
-		NSArray * perStreamDescriptions = [device streamsForDirection:kMTCoreAudioDeviceRecordDirection];
-		if (nil != perStreamDescriptions)
-		{
-			MTCoreAudioStream * stream = [perStreamDescriptions objectAtIndex:0];
-			MTCoreAudioStreamDescription * streamDesc = [stream streamDescriptionForSide:kMTCoreAudioStreamLogicalSide];
-			NSLog(@"Record direction stream %i description: %s", index, [[streamDesc description] UTF8String]);
-			NSLog(@"Record direction stream sample rate: %f", [streamDesc sampleRate]);
-			uint32 format = [streamDesc formatID];
-			NSLog(@"Record direction stream format ID: %c%c%c%c", format >> 24, ((format >> 16) & 0xFF), ((format >> 8) & 0xFF), format & 0xFF);
-		    NSLog(@"Record direction stream format flags: %i", [streamDesc formatFlags]); //defined in CoreAudioTypes.h
-			NSLog(@"Record direction stream bytes per packet: %i", [streamDesc bytesPerPacket]);
-			NSLog(@"Record direction stream frames per packet: %i", [streamDesc framesPerPacket]);
-			NSLog(@"Record direction stream bytes per frame: %i", [streamDesc bytesPerFrame]);
-			NSLog(@"Record direction stream channels per frame: %i", [streamDesc channelsPerFrame]);
-			NSLog(@"Record direction stream bits per channel: %i", [streamDesc bitsPerChannel]);
-			char * interleaved = [streamDesc isInterleaved] == TRUE ? "true" : "false"; 
-			NSLog(@"Record direction stream is interleaved: %s", interleaved);
-			basicStreamDescription->mSampleRate = [streamDesc sampleRate];
-			basicStreamDescription->mFormatID = [streamDesc formatID];
-			basicStreamDescription->mFormatFlags = [streamDesc formatFlags];
-			basicStreamDescription->mBytesPerPacket = [streamDesc bytesPerPacket];
-			basicStreamDescription->mFramesPerPacket = [streamDesc framesPerPacket];
-			basicStreamDescription->mBytesPerFrame = [streamDesc bytesPerFrame];
-			basicStreamDescription->mChannelsPerFrame = [streamDesc channelsPerFrame];
-			basicStreamDescription->mBitsPerChannel = [streamDesc bitsPerChannel];
-		}
 		inputDevice = device;
 		[inputDevice retain];
 		// set up the recording callback
 		[inputDevice setIOTarget: self
 					withSelector: @selector(readCycleForDevice:timeStamp:inputData:inputTime:outputData:outputTime:clientData:)
 				  withClientData: NULL];
+		audioFileType = kAudioFileWAVEType;
+		[self _setUpAudioFileData:device];
 	}
 	return self;
+}
+
+-(void)_setUpAudioFileData: (MTCoreAudioDevice *) device
+{
+	[self _setUpStreamDescription:device];
+	[self _setUpFSRef];
+	[self _setUpCFStringRef];
+	[self _createNewAudioFile];
+}
+
+- (void)_setUpFSRef
+{
+	//TODO: set path through file menu
+	NSLog(@"Setting up audio file");
+	char * path = "/Users/kirklandnokia";
+	audioFileDirFSPathRef = (FSRef *)calloc(1, sizeof(FSRef));
+	Boolean myDir;
+	FSPathMakeRef((UInt8 *) path, audioFileDirFSPathRef, &myDir);
+	//NSLog(@"FSRef length = %i", strlen(audioFileFSPathRef->hidden));
+	NSLog(@"Done setting up audio file");
+}
+
+- (void)_setUpCFStringRef
+{
+	//TODO: set name through file menu
+	audioFileName = CFSTR("audiofile.wav");
+	//CFShowStr(audioFileName);
+}
+
+- (void)_createNewAudioFile
+{
+	audioFileFSPathRef = (FSRef *)calloc(1, sizeof(FSRef));
+	OSStatus status = AudioFileCreate(
+		audioFileDirFSPathRef,
+		audioFileName,
+	    audioFileType,
+		basicStreamDescription,
+		kAudioFileFlags_EraseFile,
+		audioFileFSPathRef,
+		&audioFileIDHandle);
+	NSLog(@"File creation status: %i", status);
+	NSLog(@"File creation status string: %s", GetMacOSStatusErrorString(status));
+	NSLog(@"File creation comment: %s", GetMacOSStatusCommentString(status));
+	//NSLog(@"FSRef length = %i", strlen(audioFileFSPathRef->hidden));
+	NSLog(@"test %i", audioFileIDHandle);
+}
+
+- (void)_setUpStreamDescription: (MTCoreAudioDevice *) device
+{
+	basicStreamDescription = (AudioStreamBasicDescription *)calloc(1, sizeof(AudioStreamBasicDescription));
+	int index;
+	uint32 channels = [device channelsForDirection:kMTCoreAudioDeviceRecordDirection];
+	NSLog(@"device channels: %i", channels);
+	NSArray * perStreamDescriptions = [device streamsForDirection:kMTCoreAudioDeviceRecordDirection];
+	if (nil != perStreamDescriptions)
+	{
+		MTCoreAudioStream * stream = [perStreamDescriptions objectAtIndex:0];
+		MTCoreAudioStreamDescription * streamDesc = [stream streamDescriptionForSide:kMTCoreAudioStreamLogicalSide];
+		NSLog(@"Record direction stream %i description: %s", index, [[streamDesc description] UTF8String]);
+		NSLog(@"Record direction stream sample rate: %f", [streamDesc sampleRate]);
+		uint32 format = [streamDesc formatID];
+		NSLog(@"Record direction stream format ID: %c%c%c%c", format >> 24, ((format >> 16) & 0xFF), ((format >> 8) & 0xFF), format & 0xFF);
+		NSLog(@"Record direction stream format flags: %i", [streamDesc formatFlags]); //defined in CoreAudioTypes.h
+		NSLog(@"Record direction stream bytes per packet: %i", [streamDesc bytesPerPacket]);
+		NSLog(@"Record direction stream frames per packet: %i", [streamDesc framesPerPacket]);
+		NSLog(@"Record direction stream bytes per frame: %i", [streamDesc bytesPerFrame]);
+		NSLog(@"Record direction stream channels per frame: %i", [streamDesc channelsPerFrame]);
+		NSLog(@"Record direction stream bits per channel: %i", [streamDesc bitsPerChannel]);
+		char * interleaved = [streamDesc isInterleaved] == TRUE ? "true" : "false"; 
+		NSLog(@"Record direction stream is interleaved: %s", interleaved);
+		basicStreamDescription->mSampleRate = [streamDesc sampleRate];
+		basicStreamDescription->mFormatID = [streamDesc formatID];
+		basicStreamDescription->mFormatFlags = [streamDesc formatFlags];
+		basicStreamDescription->mBytesPerPacket = [streamDesc bytesPerPacket];
+		basicStreamDescription->mFramesPerPacket = [streamDesc framesPerPacket];
+		basicStreamDescription->mBytesPerFrame = [streamDesc bytesPerFrame];
+		basicStreamDescription->mChannelsPerFrame = [streamDesc channelsPerFrame];
+		basicStreamDescription->mBitsPerChannel = [streamDesc bitsPerChannel];
+	}
 }
 
 - (void)dealloc
 {
 	free(basicStreamDescription);
+	free(audioFileFSPathRef);
 	NSLog(@"Deallocating audioqueue");
 	[inputDevice release];
 	[super dealloc];
